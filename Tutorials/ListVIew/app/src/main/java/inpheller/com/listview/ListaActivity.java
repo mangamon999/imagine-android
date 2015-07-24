06f5f79a7b6ef75c56a2c6403a40f88ad5255df5
Bruno Pinheiro <brunopinheiro@Brunos-MacBook-Pro.local>
2015-07-23 22:23:40 -0300

    Implemented ListView with custom item layout and action button to add new items

--- Tutorials/ListVIew/app/src/main/java/inpheller/com/listview/ListaActivity.java
+++ Tutorials/ListVIew/app/src/main/java/inpheller/com/listview/ListaActivity.java
@@ -4,34 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListaActivity extends ActionBarActivity {
+
+    private ArrayList<String> itens;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+        ListView listaItens = (ListView) findViewById(R.id.lista_itens);
+
+
+        itens = new ArrayList<String>();
+
+        itens.add("Tesoura");
+        itens.add("Caderno");
+        itens.add("Lapis");
+        itens.add("Borracha");
+        itens.add("Apontador");
+
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.lista_item, R.id.lista_item_nome, itens);
+
+        listaItens.setAdapter(arrayAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lista, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void adicionaNovoItem(View view) {
+        itens.add("Novo item");
+        arrayAdapter.notifyDataSetChanged();
     }
 }
